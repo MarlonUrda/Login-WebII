8338c84fd139b85c964e360af6c59d283bd270de
MarlonUrda <MGUR2025pro@gmail.com>
2024-06-11 17:53:49 -0700

    some verifications to register

--- Backend/odk/register.js
+++ Backend/odk/register.js
@@ -8,6 +8,21 @@ export const regApi = async (req, res) => {
 
   const values = [email, passwordEncrypt, username];
   try {
+    const em = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    const pass = await pool.query("SELECT * FROM users WHERE password = $1", [
+      password,
+    ]);
+
+    if (em.rows.length > 0) {
+      res.status(400).json({ message: "El email ya existe" });
+    }
+
+    if (pass.rows.length > 0) {
+      res.status(400).json({ message: "La contraseña ya existe" });
+    }
+
     const result = await pool.query(
       "INSERT INTO users (email, password, name) VALUES ($1, $2, $3)",
       values
