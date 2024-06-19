066a7ea78613fb0eb6b189e4817fcb73da441474
MarlonUrda <MGUR2025pro@gmail.com>
2024-06-19 11:24:25 -0700

    nombre de carpeta cambiado

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ import { getConnection, pool } from "./database/pool.js";
 import { login } from "./odk/login.js";
 import { regApi } from "./odk/register.js";
 import cors from "cors";
-import { emailapi } from "./odk/forgot.js"; 
+import { emailapi } from "./odk/forgot.js";
 import { userData } from "./odk/home.js";
 import { encrypt } from "./odk/encrypt.js";
 
@@ -19,10 +19,12 @@ const app = express();
 const host = "localhost";
 const port = 3000;
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 getConnection();
 
@@ -31,8 +33,8 @@ app.use(
     secret: "secret",
     resave: false,
     saveUninitialized: false,
-    cookie: { 
-      maxAge: 600000, 
+    cookie: {
+      maxAge: 600000,
       sameSite: false,
     },
   })
@@ -40,17 +42,12 @@ app.use(
 
 app.use(express.json());
 
-
 app.use(express.static(parentDirectory + "/client/dist"));
-
-
 
 app.get("/user-data", (req, res) => {
   console.log("userData");
   userData(req, res);
-
 });
-
 
 app.post("/emailProccess", (req, res) => {
   console.log("req.body.email");
@@ -68,53 +65,71 @@ app.post("/toProcess", async (req, res) => {
   res.status(200);
 });
 
-app.post("/register",  (req, res) => {
+app.post("/register", (req, res) => {
   regApi(req, res);
 });
 
-app.get('/reset/:token', async (req, res) => {
+app.get("/reset/:token", async (req, res) => {
   const { token } = req.params;
   console.log(token);
 
   try {
-    const result = await pool.query('SELECT * FROM users WHERE resetPasswordToken = $1 AND resetPasswordExpires > $2', [token, Date.now()]);
+    const result = await pool.query(
+      "SELECT * FROM users WHERE resetPasswordToken = $1 AND resetPasswordExpires > $2",
+      [token, Date.now()]
+    );
     if (!(result.rows.length > 0)) {
-      return res.status(400).json({ message: 'El token de restablecimiento de contraseña es inválido o ha expirado.' });
+      return res
+        .status(400)
+        .json({
+          message:
+            "El token de restablecimiento de contraseña es inválido o ha expirado.",
+        });
     }
     // Obtén el token del resultado
     const user = result.rows[0];
     const tokenFromDatabase = user.resetpasswordtoken; // Aquí cambiamos 'resetPasswordToken' por 'resetpasswordtoken'
 
-
     console.log(tokenFromDatabase);
-  }catch (error) {
+  } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error al procesar la solicitud.' });
+    res.status(500).json({ message: "Error al procesar la solicitud." });
   }
 });
 
-app.post('/reset/:token', async (req, res) => {
+app.post("/reset/:token", async (req, res) => {
   const { token } = req.params;
   const { password } = req.body;
   console.log(token);
-  console.log(password  );
+  console.log(password);
 
   try {
-    const result = await pool.query('SELECT * FROM users WHERE resetPasswordToken = $1 AND resetPasswordExpires > $2', [token, Date.now()]);
+    const result = await pool.query(
+      "SELECT * FROM users WHERE resetPasswordToken = $1 AND resetPasswordExpires > $2",
+      [token, Date.now()]
+    );
     if (!(result.rows.length > 0)) {
-      return res.status(400).json({ message: 'El token de restablecimiento de contraseña es inválido o ha expirado.' });
+      return res
+        .status(400)
+        .json({
+          message:
+            "El token de restablecimiento de contraseña es inválido o ha expirado.",
+        });
     }
 
     const user = result.rows;
 
     const hashedPassword = await encrypt(password, 10);
-    await pool.query('UPDATE users SET password = $1, resetPasswordToken = NULL, resetPasswordExpires = NULL WHERE email = $2', [hashedPassword, user[0].email]);
+    await pool.query(
+      "UPDATE users SET password = $1, resetPasswordToken = NULL, resetPasswordExpires = NULL WHERE email = $2",
+      [hashedPassword, user[0].email]
+    );
 
-    res.json({ message: 'Contraseña restablecida con éxito.' });
+    res.json({ message: "Contraseña restablecida con éxito." });
   } catch (error) {
-  console.error(error);
-  res.status(500).json({ message: 'Error al restablecer la contraseña.' });
-}
+    console.error(error);
+    res.status(500).json({ message: "Error al restablecer la contraseña." });
+  }
 });
 
 app.use((req, res) => {
@@ -124,8 +139,3 @@ app.use((req, res) => {
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
-
-
-
-
-
