c69a46f8394dbaac7e8f1fc316bd73082b71f185
MarlonUrda <MGUR2025pro@gmail.com>
2024-06-19 11:42:45 -0700

    pool

--- Backend/database/pool.js
+++ Backend/database/pool.js
@@ -6,17 +6,11 @@ const result = dotenv.config({ path: "./env/.env" });
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  // port: process.env.DB_PORT,
-
-  user: "postgres",
-  host: "localhost",
-  password: "123456789",
-  database: "Login",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 export const getConnection = () => {
