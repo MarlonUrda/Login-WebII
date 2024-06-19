1ef7093d3a619dde8d5f95324f77f53980feca4f
Atla004 <andrese.g.v.13579@gmail.com>
2024-06-18 21:19:04 -0400

    ˋ︿ˊ

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,13 @@ import session from "express-session";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import morgan from "morgan";
-import { getConnection } from "./database/pool.js";
+import { getConnection, pool } from "./database/pool.js";
 import { login } from "./odk/login.js";
 import { regApi } from "./odk/register.js";
 import cors from "cors";
 import { emailapi } from "./odk/forgot.js"; 
 import { userData } from "./odk/home.js";
+import { encrypt } from "./odk/encrypt.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -71,8 +72,49 @@ app.post("/register",  (req, res) => {
   regApi(req, res);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(parentDirectory + '/client/dist/index.html');
+app.get('/reset/:token', async (req, res) => {
+  const { token } = req.params;
+  console.log(token);
+
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE resetPasswordToken = $1 AND resetPasswordExpires > $2', [token, Date.now()]);
+    if (!(result.rows.length > 0)) {
+      return res.status(400).json({ message: 'El token de restablecimiento de contraseña es inválido o ha expirado.' });
+    }
+    // Obtén el token del resultado
+    const user = result.rows[0];
+    const tokenFromDatabase = user.resetpasswordtoken; // Aquí cambiamos 'resetPasswordToken' por 'resetpasswordtoken'
+
+    // Imprime el token en la consola
+    console.log(tokenFromDatabase);
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al procesar la solicitud.' });
+  }
+});
+
+app.post('/reset/:token', async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+  console.log(token);
+  console.log(password  );
+
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE resetPasswordToken = $1 AND resetPasswordExpires > $2', [token, Date.now()]);
+    if (!(result.rows.length > 0)) {
+      return res.status(400).json({ message: 'El token de restablecimiento de contraseña es inválido o ha expirado.' });
+    }
+
+    const user = result.rows;
+
+    const hashedPassword = await encrypt(password, 10);
+    await pool.query('UPDATE users SET password = $1, resetPasswordToken = NULL, resetPasswordExpires = NULL WHERE email = $2', [hashedPassword, user[0].email]);
+
+    res.json({ message: 'Contraseña restablecida con éxito.' });
+  } catch (error) {
+  console.error(error);
+  res.status(500).json({ message: 'Error al restablecer la contraseña.' });
+}
 });
 
 app.use((req, res) => {
@@ -82,5 +124,8 @@ app.use((req, res) => {
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
+
+
+
 
 
--- Backend/odk/forgot.js
+++ Backend/odk/forgot.js
@@ -3,6 +3,7 @@
 import { pool } from "../database/pool.js";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import crypto from 'crypto';
 
 const result = dotenv.config({ path: "./env/.env" });
 
@@ -14,37 +15,42 @@ export const emailapi = async (req, res) => {
       email,
     ]);
 
-    if (em.rows.length > 0) {
-      console
-      //El email ya existe
-      let transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: true,
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS,
-        }
-      });
-      
-      let mailOptions = {
-        from: process.env.SMTP_USER,
-        to: email,
-        subject: 'Se te olvido tu contraseña?', 
-        text: 'Bobo,entra al link para cambiar tu contraseña:'
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-           res.status(201).json({ message: `El email si existe Bobo2 ${email}` });
-        }
-      });
-
-    }else{
-      return res.status(400).json({ message: `El email no existe Bobo2 ${email}` });
+    if(!(em.rows.length > 0)){
+      return res.status(404).json({ message: `El email no existe Bobo2 ${email}` });
     }
+
+    const token = crypto.randomBytes(20).toString('hex');
+    const expires = Date.now() + 3600000; // 1 hour
+
+    await pool.query('UPDATE users SET resetPasswordToken = $1, resetPasswordExpires = $2 WHERE email = $3', [token, expires, email]);
+
+    let transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      }
+    });
+    
+    let mailOptions = {
+      from: process.env.SMTP_USER,
+      to: email,
+      subject: 'Se te olvido tu contraseña?', 
+      text: `Bobo,entra al link para cambiar tu contraseña:\n\n
+             http://${req.headers.host}/reset/${token}\n\n
+             Si tú no solicitaste esto, por favor ignora este correo y tu contraseña permanecerá sin cambios.\n`,
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        return console.log(error);
+      } 
+      
+      res.status(201).json({ message: `El email si existe Bobo2 ${email}` });
+      
+    });
 
 
 
