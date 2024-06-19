40e972e4fa7c6ce870aa7660457ae6bcd6db24ba
JoseCarrilloURU <jose.30739823@uru.edu>
2024-06-18 22:53:37 -0400

    password2

--- client/src/views/NewPass.vue
+++ client/src/views/NewPass.vue
@@ -11,19 +11,19 @@ import {
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 
-const email = ref("");
+const password = ref("");
 
-const sendEmail = async () => {
-  console.log("sendEmail");
-  console.log(email.value);
+const sendPassword = async () => {
+  console.log("sendPassword");
+  console.log(password.value);
 
-  const response = await fetch("http://localhost:3000/emailProccess", {
+  const response = await fetch("http://localhost:3000/reset/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      email: email.value,
+      password: password.value,
     }),
   });
   console.log("response");
@@ -49,28 +49,28 @@ const sendEmail = async () => {
     </CardHeader>
     <CardContent>
       <div class="grid gap-4">
-        <div class="mt-[12%] grid gap-2">
-            <Label for="email">Nueva Contraseña</Label>
+        <div class="mt-[15%] grid gap-2">
+            <Label for="password">Nueva Contraseña</Label>
           <Input
-            v-model="email"
-            id="mail"
+            v-model="password"
+            id="password"
             type="text"
             placeholder="Introduce tu nueva contraseña"
             required
           />
         </div>
-        <div class="mt-[2%] grid gap-2">
-            <Label for="email">Comprobación de Contraseña</Label>
+        <!-- <div class="mt-[2%] grid gap-2">
+            <Label for="password">Comprobación de Contraseña</Label>
           <Input
-            v-model="email"
-            id="mail"
+            v-model="password"
+            id="password2"
             type="text"
             placeholder="Ambas contraseñas deben coincidir"
             required
           />
-        </div>
+        </div> -->
 
-        <Button  @click="sendEmail" type="submit" class="w-full mt-[2%]">
+        <Button  @click="sendPassword" type="submit" class="w-full mt-[1.5%]">
           Enviar
         </Button>
       </div>
