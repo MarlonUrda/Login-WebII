25cf2e86cc720c6fcb9df878aa026bcb9ed1e8c6
Atla004 <andrese.g.v.13579@gmail.com>
2024-06-22 20:13:30 -0400

    ?

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,6 +1,4 @@
 <script setup >
-import { ref } from "vue";
-import { Button } from "../components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,54 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "../components/ui/card";
-import { Input } from "../components/ui/input";
-import { Label } from "../components/ui/label";
-import { useRouter } from 'vue-router';
 
-const email = ref("");
-const password = ref("");
-const username = ref("");
-const errorMessage = ref("");
-const router = useRouter();
-
-const sendUser = async () => {
-  console.log("sendUser");
-
-  try{
-
-    const response = await fetch("http://localhost:3000/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email.value,
-        password: password.value,
-        username: username.value,
-      }),
-    })
-
-
-
-    const data = await response.json();
-
-    console.log(data);
-    
-
-    if (response.status > 300) {
-      errorMessage.value = data.message;
-    } else {
-      errorMessage.value = "";
-      router.push({name:'Registered',  params: { email:email.value, password:password.value,username:username.value } });
-    }
-    email.value = "";
-    password.value = "";
-    username.value = "";
-
-  }catch(err){
-    console.log(err)
-  }
-};
 </script>
 
 <template>
