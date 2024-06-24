2457cf37ee60b2f4950fbf460cc339fe347301ab
MarlonUrda <MGUR2025pro@gmail.com>
2024-06-24 11:27:24 -0700

    empezando security

--- Backend/subsystem/encrypt.js
+++ Backend/subsystem/encrypt.js
@@ -1,10 +1,10 @@
 import bc from "bcryptjs";
 
 /**
-* Encripta una contraseña utilizando bcryptjs.
-* @param {string} password La contraseña en texto plano que se desea encriptar.
-* @returns {Promise<string>} Una promesa que resuelve con el hash encriptado de la contraseña.
-*/
+ * Encripta una contraseña utilizando bcryptjs.
+ * @param {string} password La contraseña en texto plano que se desea encriptar.
+ * @returns {Promise<string>} Una promesa que resuelve con el hash encriptado de la contraseña.
+ */
 export const encrypt = async (password) => {
   const salt = await bc.genSalt(10);
   const hash = await bc.hash(password, salt);
@@ -18,5 +18,10 @@ export const encrypt = async (password) => {
  * @returns {Promise<boolean>} Una promesa que resuelve con un booleano que indica si la contraseña en texto plano coincide con el hash encriptado.
  */
 export const compare = async (password, encrypted) => {
-  return await bc.compare(password, encrypted);
+  try {
+    const match = await bc.compare(password, encrypted);
+    return match;
+  } catch (error) {
+    console.log("La contraseña no coincide");
+  }
 };
